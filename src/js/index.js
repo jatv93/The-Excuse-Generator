1381f45968da226436c0bc0b2cef508734b42173
jatv93 <toledojennifer20@gmail.com>
2020-05-04 00:18:24 +0000

    primer commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,20 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var quien = who[Math.round(Math.random() * (who.length - 1))];
+  var que = what[Math.round(Math.random() * (what.length - 1))];
+  var cuando = when[Math.round(Math.random() * (when.length - 1))];
+
+  document.getElementById("excuse").innerHTML =
+    "<p>" + quien + " " + que + " " + cuando + ".</p>";
 };
